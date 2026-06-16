0480241dc16b0a02eec0b7fef60c826699d20a11
Kieran Osgood <kieran.osgood@shopify.com>
2026-06-16 12:50:04 +0100

    Align PassKit protocol abstractions with Swift 6 (#90)
    ### What changes are you making?
    
    <!-- Please describe why you are making these changes -->
    
    ### How to test
    
    <!-- Please outline the steps to test your changes -->
    
    ---
    
    ### Before you merge
    
    > [!IMPORTANT]
    >
    > - [ ] I've added tests to support my implementation
    > - [ ] I have read and agree with the [Contribution Guidelines](./CONTRIBUTING.md)
    > - [ ] I have read and agree with the [Code of Conduct](./CODE_OF_CONDUCT.md)
    > - [ ] I've updated the relevant platform README (`platforms/swift/README.md` and/or `platforms/android/README.md`)
    
    ---
    
    <details>
    <summary>Releasing a new Swift version?</summary>
    
    - [ ] I have bumped the version in `platforms/swift/ShopifyCheckoutKit.podspec`
    - [ ] I have bumped the version in `platforms/swift/Sources/ShopifyCheckoutKit/ShopifyCheckoutKit.swift`
    - [ ] I have updated `platforms/swift/CHANGELOG.md`
    - [ ] I have updated the SwiftPM/CocoaPods version snippets in `platforms/swift/README.md` (major version only)
    
    </details>
    
    <details>
    <summary>Releasing a new Android version?</summary>
    
    - [ ] I have bumped the `versionName` in `platforms/android/lib/build.gradle`
    - [ ] I have updated `platforms/android/CHANGELOG.md`
    - [ ] I have updated the Gradle/Maven version snippets in `platforms/android/README.md`
    
    </details>
    
    > [!TIP]
    > See the [Contributing documentation](./CONTRIBUTING.md) for the full release process per platform.

--- platforms/swift/Sources/ShopifyAcceleratedCheckouts/Wallets/ApplePay/ApplePayAuthorizationDelegate/ApplePayAuthorizationDelegate.swift
+++ platforms/swift/Sources/ShopifyAcceleratedCheckouts/Wallets/ApplePay/ApplePayAuthorizationDelegate/ApplePayAuthorizationDelegate.swift
@@ -6,16 +6,17 @@ import ShopifyCheckoutKit
 
 /// Protocol to abstract PKPaymentAuthorizationController for testing
 @available(iOS 16.0, *)
+@MainActor
 protocol PaymentAuthorizationController {
-    var delegate: PKPaymentAuthorizationControllerDelegate? { get set }
+    var delegate: (any PKPaymentAuthorizationControllerDelegate)? { get set }
     func present() async -> Bool
-    func dismiss(completion: (() -> Void)?)
+    func dismiss(completion: (@Sendable () -> Void)?)
 }
 
 extension PKPaymentAuthorizationController: PaymentAuthorizationController {}
 
 @available(iOS 16.0, *)
-typealias PKAuthorizationControllerFactory = (PKPaymentRequest) -> PaymentAuthorizationController
+typealias PKAuthorizationControllerFactory = @MainActor @Sendable (PKPaymentRequest) -> any PaymentAuthorizationController
 
 @available(iOS 16.0, *)
 @MainActor
