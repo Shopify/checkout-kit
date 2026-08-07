863f21931bcc46196081ce271e58f9d14f070817
Daniel Kift <daniel.kift@shopify.com>
2026-08-07 14:11:06 +0100

    Enable file naming lint rules
    Assisted-By: devx/e2f77bde-d08f-41c4-a14f-b50a215d784a

---
+++ platforms/android/samples/CheckoutKitAndroidDemo/app/src/main/java/com/shopify/checkoutkit/androiddemo/common/navigation/Screen.kt
@@ -0,0 +1,55 @@
+package com.shopify.checkoutkit.androiddemo.common.navigation
+
+import androidx.navigation.NavBackStackEntry
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+sealed class Screen(val route: String) {
+    data object Home : Screen("home")
+    data object Product : Screen("product/{productId}") {
+        fun productIdRouteVariable(backStackEntry: NavBackStackEntry): String {
+            return backStackEntry.arguments?.getString("productId") ?: ""
+        }
+
+        fun route(productId: String): String {
+            return route.replace("{productId}", URLEncoder.encode(productId, StandardCharsets.UTF_8.name()))
+        }
+    }
+
+    data object Products : Screen("product")
+    data object ProductCollection : Screen("collection/{collectionHandle}") {
+        fun collectionHandleRouteVariable(backStackEntry: NavBackStackEntry): String {
+            return backStackEntry.arguments?.getString("collectionHandle") ?: ""
+        }
+
+        fun route(collectionHandle: String): String {
+            return route.replace(
+                "{collectionHandle}",
+                URLEncoder.encode(collectionHandle, StandardCharsets.UTF_8.name())
+            )
+        }
+    }
+
+    data object Cart : Screen("cart")
+    data object Settings : Screen("settings")
+    data object Logs : Screen("logs")
+    data object Login : Screen("login")
+    data object Account : Screen("account")
+
+    companion object {
+        fun fromRoute(route: String): Screen {
+            return when (route) {
+                Home.route -> Home
+                ProductCollection.route -> ProductCollection
+                Product.route -> Product
+                Products.route -> Products
+                Cart.route -> Cart
+                Settings.route -> Settings
+                Logs.route -> Logs
+                Login.route -> Login
+                Account.route -> Account
+                else -> throw RuntimeException("Unknown route")
+            }
+        }
+    }
+}
