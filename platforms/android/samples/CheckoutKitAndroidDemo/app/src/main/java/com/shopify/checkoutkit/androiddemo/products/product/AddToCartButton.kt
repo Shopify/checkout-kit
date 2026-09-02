2720a3e19b94928e018ef032b337052984bc90eb
Dan Kift <daniel.kift@shopify.com>
2026-09-02 10:40:29 +0100

    Enable unused parameter and require lint rules (#612)
    Assisted-By: devx/62a938bf-9923-4194-bbbf-562a92be16c5

--- platforms/android/samples/CheckoutKitAndroidDemo/app/src/main/java/com/shopify/checkoutkit/androiddemo/products/product/AddToCartButton.kt
+++ platforms/android/samples/CheckoutKitAndroidDemo/app/src/main/java/com/shopify/checkoutkit/androiddemo/products/product/AddToCartButton.kt
@@ -1,18 +1,16 @@
 package com.shopify.checkoutkit.androiddemo.products.product
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.TextButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
 import com.shopify.checkoutkit.androiddemo.R
 import com.shopify.checkoutkit.androiddemo.common.components.Header3
 
@@ -23,21 +21,23 @@ fun AddToCartButton(
     modifier: Modifier,
     onClick: () -> Unit
 ) {
+    val buttonEnabled = enabled && !loading
+
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        TextButton(
+        OutlinedButton(
             modifier = Modifier.fillMaxWidth(),
-            enabled = !loading,
+            enabled = buttonEnabled,
             onClick = onClick,
-            border = BorderStroke(1.dp, MaterialTheme.colorScheme.onBackground),
-            shape = RectangleShape
+            shape = RectangleShape,
         ) {
             Box {
                 Header3(
                     textAlign = TextAlign.Center,
                     text = stringResource(id = R.string.product_add_to_cart),
+                    color = LocalContentColor.current,
                 )
             }
         }
