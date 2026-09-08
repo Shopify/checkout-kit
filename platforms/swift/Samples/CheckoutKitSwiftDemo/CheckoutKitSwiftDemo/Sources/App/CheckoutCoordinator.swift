5482374a7a498344e40d526f2c2a2e850aa1a20a
Mark Murray <mark.murray@shopify.com>
2026-09-08 12:16:35 +0100

    Prototype protocol-agnostic checkout events

--- platforms/swift/Samples/CheckoutKitSwiftDemo/CheckoutKitSwiftDemo/Sources/App/CheckoutCoordinator.swift
+++ platforms/swift/Samples/CheckoutKitSwiftDemo/CheckoutKitSwiftDemo/Sources/App/CheckoutCoordinator.swift
@@ -1,5 +1,3 @@
-import EmbeddedCheckoutProtocol
-import OSLog
 import ShopifyCheckoutKit
 import UIKit
 
@@ -8,15 +6,6 @@ class CheckoutCoordinator: UIViewController {
     var root: UIViewController?
 
     private let checkoutDelegate = CartResettingCheckoutDelegate()
-    private lazy var client = CheckoutProtocol.Client()
-        .on(CheckoutProtocol.start) { checkout in
-            OSLogger.shared.debug("[UCP] Checkout started: \(checkout.id)")
-        }
-        .on(CheckoutProtocol.complete) { [checkoutDelegate] checkout in
-            OSLogger.shared.debug("[UCP] Checkout completed: \(checkout.order?.id ?? "unknown")")
-            checkoutDelegate.markCompleted()
-        }
-
     init(window: UIWindow?) {
         self.window = window
         super.init(nibName: nil, bundle: nil)
@@ -31,7 +20,7 @@ class CheckoutCoordinator: UIViewController {
 
     public func present(checkout url: URL) {
         if let rootViewController = window?.topMostViewController() {
-            ShopifyCheckoutKit.present(checkout: url, from: rootViewController, delegate: checkoutDelegate, client: client)
+            ShopifyCheckoutKit.present(checkout: url, from: rootViewController, delegate: checkoutDelegate)
             root = rootViewController
         }
     }
