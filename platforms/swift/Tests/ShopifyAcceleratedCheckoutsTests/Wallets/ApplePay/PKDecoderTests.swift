0b6da87ae535a708d6b1977c22ade5952c694131
Mark Murray <mark.murray@shopify.com>
2026-09-16 16:32:55 +0100

    Migrate accelerated checkout discounts to cart applications

--- platforms/swift/Tests/ShopifyAcceleratedCheckoutsTests/Wallets/ApplePay/PKDecoderTests.swift
+++ platforms/swift/Tests/ShopifyAcceleratedCheckoutsTests/Wallets/ApplePay/PKDecoderTests.swift
@@ -38,8 +38,7 @@ class PKDecoderTests: XCTestCase {
                 totalTaxAmount: nil,
                 totalDutyAmount: nil
             ),
-            discountCodes: [],
-            discountAllocations: []
+            discountApplications: []
         )
     }
 
